e4dcc95822168fe084c322d526ff078defbb9816
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-06 09:28:07 -0600

    Set up many to many relationship between machine and snacks

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   scenario 'they see the location of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    
 
-    visit machine_path(dons)
-
-    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    # visit machine_path(dons)
+    #
+    # expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 end
---
+++ spec/features/vending_machine_spec/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Vending machine show page', type: :feature do
+  scenario 'can see name and price of all snacks associated with machine' do
+    # owner = Owner.create(name: "Sam's Snacks")
+    # dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    #
+    # visit machine_path(dons)
+    #
+    # expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+end
