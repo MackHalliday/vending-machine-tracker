63e521469cd95c4455a7bb031d796ee29ef5fd47
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-06 09:53:26 -0600

    Finish user story 2

--- spec/features/vending_machine_spec/show_spec.rb
+++ spec/features/vending_machine_spec/show_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Vending machine show page', type: :feature do
+  before :each do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons  = @owner.machines.create!(location: "Don's Mixed Drinks")
+
+    @candy_bar = @dons.snacks.create!(name: "Candy Bar", price: 1.50)
+    @chips = @dons.snacks.create!(name: "Chips", price: 2.00)
+    @gum = @dons.snacks.create!(name: "Gum", price: 0.50)
+    @pizza = @dons.snacks.create!(name: "Pizza", price: 3.50)
+  end
+
   scenario 'can see name and price of all snacks associated with machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    visit (machine_path(@dons))
 
-    candy_bar = dons.snacks.create!(name: "Candy Bar", price: 1.50)
-    chips = dons.snacks.create!(name: "Chips", price: 2.00)
-    gum = dons.snacks.create!(name: "Gum", price: 0.50)
-    pizza = dons.snacks.create!(name: "Pizza", price: 3.50)
+    expect(page).to have_content("#{@candy_bar.name}")
+    expect(page).to have_content("#{@candy_bar.price}")
+    expect(page).to have_content("#{@chips.name}")
+    expect(page).to have_content("#{@candy_bar.price}")
+    expect(page).to have_content("#{@gum.name}")
+    expect(page).to have_content("#{@gum.price}")
+    expect(page).to have_content("#{@pizza.name}")
+    expect(page).to have_content("#{@pizza.name}")
+  end
 
-    visit (machine_path(dons))
+#   As a visitor
+# When I visit a vending machine show page
+# I also see an average price for all of the snacks in that machine
+  it "can show the average price of snacks" do
+    visit (machine_path(@dons))
 
-    expect(page).to have_content("#{candy_bar.name}")
-    expect(page).to have_content("#{candy_bar.price}")
-    expect(page).to have_content("#{chips.name}")
-    expect(page).to have_content("#{candy_bar.price}")
-    expect(page).to have_content("#{gum.name}")
-    expect(page).to have_content("#{gum.price}")
-    expect(page).to have_content("#{pizza.name}")
-    expect(page).to have_content("#{pizza.name}")
+    expect(page).to have_content("Average Price: 1.88")
   end
 end
